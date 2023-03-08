40bad88f16f8687e03eeeea5425a81d353e29b38
Iker Garcia <81225623+Iker-hub@users.noreply.github.com>
2023-03-08 08:59:22 +0100

    Upload files

--- js/sunanimation/main.js
+++ js/sunanimation/main.js
@@ -1,4 +1,13 @@
+const INITIAL_POSITION_SUN_X = 200;
+const INITIAL_POSITION_SUN_Y = -40;
+const END_POSITION_SUN_X = 378;
+const END_POSITION_SUN_Y = 43.23;
+const DELAY = 10000;
+
 let sunAnimationContainer = document.querySelector("#sunanimation-container");
+
+var solarPanel;
+var sun;
 
 var game = new Phaser.Game(
   sunAnimationContainer.offsetWidth,
@@ -13,31 +22,22 @@ var game = new Phaser.Game(
 );
 
 function preload() {
-  game.load.image("solarPanel", "./js/sunanimation/solar-panel.svg");
+  game.load.image("solarPanel", "./js/sunanimation/solar-panel.png");
   game.load.image("sun", "./js/sunanimation/sun.svg");
 }
-
-var solarPanel;
-var orb;
-
-const INITIAL_POSITION_SUN_X = 200;
-const INITIAL_POSITION_SUN_Y = -40;
-const END_POSITION_SUN_X = 378;
-const END_POSITION_SUN_Y = 43.23;
-const DELAY = 10000;
 
 function create() {
   game.stage.backgroundColor = "#add8e6";
 
   solarPanel = game.add.sprite(0, 0, "solarPanel");
-  solarPanel.anchor.setTo(0.3, 0.7);
+  solarPanel.anchor.setTo(0.5);
 
   game.physics.arcade.enable(solarPanel);
 
-  orb = game.add.sprite(0, 0, "sun");
-  orb.anchor.setTo(0.75);
-  orb.pivot.x = INITIAL_POSITION_SUN_X;
-  orb.pivot.y = INITIAL_POSITION_SUN_Y;
+  sun = game.add.sprite(0, 0, "sun");
+  sun.anchor.setTo(0.5);
+  sun.pivot.x = INITIAL_POSITION_SUN_X;
+  sun.pivot.y = INITIAL_POSITION_SUN_Y;
 }
 
 function update() {
@@ -46,10 +46,10 @@ function update() {
 }
 
 function moveSun(endPositionX, endPositionY, game) {
-  if (orb.getBounds().x < endPositionX && orb.getBounds().y > endPositionY) {
-    orb.rotation += 0.005;
+  if (sun.getBounds().x < endPositionX && sun.getBounds().y > endPositionY) {
+    sun.rotation += 0.005;
   } else {
-    orb.rotation == 0;
+    sun.rotation == 0;
     game.time.events.add(
       DELAY,
       function () {
@@ -61,14 +61,14 @@ function moveSun(endPositionX, endPositionY, game) {
 }
 
 function preRender() {
-  orb.x = solarPanel.x;
-  orb.y = solarPanel.y;
+  sun.x = solarPanel.x;
+  sun.y = solarPanel.y;
 }
 
 function resizeAnimation(game) {
   game.scale.setGameSize(sunAnimationContainer.offsetWidth, 600);
   solarPanel.x = game.width / 2 + 20;
   solarPanel.y = game.height / 2;
-  orb.x = game.width / 2;
-  orb.y = game.height / 2;
+  sun.x = game.width / 2;
+  sun.y = game.height / 2;
 }
